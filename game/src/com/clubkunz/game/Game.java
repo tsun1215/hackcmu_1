3482a5ffe8cb9edf41321c02aa4d4262c7dbed72
davidlindenbaum <dlindenb@andrew.cmu.edu>
2013-09-14 16:03:46 -0400

    preliminary location stuff

--- game/src/com/clubkunz/game/Game.java
+++ game/src/com/clubkunz/game/Game.java
@@ -1,18 +1,68 @@
 package com.clubkunz.game;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Game extends Activity {
+	LocationManager location;
+	int updateFreq = 1000;
+	boolean hasLocation = false;
+	Location currentLocation;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_game);
+		
+		location = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Criteria c = new Criteria();
+		c.setAccuracy(Criteria.ACCURACY_FINE);
+		location.requestLocationUpdates("gps", updateFreq, 0, new LocationListener() {
+			
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderDisabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onLocationChanged(Location location) {
+				currentLocation = location;
+			}
+		});
 
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
+		Button b = (Button)controlsView.findViewById(R.id.set_bomb);
+		b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
 	}
 }
