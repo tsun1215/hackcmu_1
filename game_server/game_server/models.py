56522c9bb7f30d631f87fe5bb1b008d4a7c1be5f
Terence Sun <tsun1215@gmail.com>
2013-09-14 19:01:13 -0400

    Added more method stubs, get_bombs is working

--- game_server/game_server/models.py
+++ game_server/game_server/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import utc
 
 
 class Game(models.Model):
@@ -14,17 +15,22 @@ class Player(models.Model):
     username = models.CharField(max_length=100, unique=True)
     device_id = models.CharField(max_length=2000, unique=True)
 
-    game = models.ForeignKey(Game)
-
     first_name = models.CharField(max_length=100, default="")
     last_name = models.CharField(max_length=100, default="")
+
+    # Current game information
+    game = models.ForeignKey(Game, null=True)
 
     last_longitude = models.FloatField(null=True, blank=True)
     last_latitude = models.FloatField(null=True, blank=True)
     last_altitude = models.FloatField(null=True, blank=True)
     last_seen = models.DateTimeField(null=True, blank=True)
-    
+
     bombs_remaining = models.IntegerField(default=1)
+
+    hitlist = models.CharField(blank=True, max_length=2000)
+    target = models.CharField(blank=True, max_length=2000)
+    friendlys = models.CharField(blank=True, max_length=2000)
 
 
     class Meta:
@@ -33,6 +39,15 @@ class Player(models.Model):
 
     def __unicode__(self):
         return self.username
+
+    def clear_game_info(self):
+        self.last_longitude = None
+        self.last_latitude = None
+        self.last_altitude = None
+        self.last_seen = None
+        hitlist = target = friendlys = ""
+        self.bombs_remaining = 1
+        self.save()
 
     def get_last_loc(self):
         return self.last_longitude, self.last_latitude, self.last_altitude
@@ -63,3 +78,14 @@ class Bomb(models.Model):
 
     def __unicode__(self):
         return "(%s) <%.3f, %.3f, %.3f>" % (self.placed_by.username, self.longitude, self.latitude, self.altitude)
+
+    def get_info(self):
+        return {"longitude": self.longitude, "latitude:": self.latitude, 
+                "altitude": self.altitude, "placed_by": self.placed_by.device_id, 
+                "time_placed": self.time_placed.replace(tzinfo=utc).isoformat(),
+                "radius": self.radius}
+
+
+def bombs_in_radius(longitude, latitude, radius):
+    return Bomb.objects.filter(longitude__gte=longitude-radius, longitude__lte=longitude+radius,
+                               latitude__gte=latitude-radius, latitude__lte=latitude+radius)
--- game_server/game_server/views.py
+++ game_server/game_server/views.py
@@ -1,22 +1,24 @@
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.core.exceptions import ObjectDoesNotExist
 import simplejson
 import datetime
 from django.utils.timezone import utc
+from game_server.models import *
 
 
 def test(request):
-	import random
-	locs = []
-	for x in range(5):
-		locs.append({
-			"long": random.uniform(-180,180),
-			"lat": random.uniform(-90, 90),
-			"altitude": random.uniform(0, 100),
-			"time_placed": (datetime.datetime.utcnow().replace(tzinfo=utc) - datetime.timedelta(hours=random.randint(0, 120))).isoformat(),
-			"radius": 10,
-			"placed_by": "239r2e90efj%d" % random.randint(5,8),
-		})
-	return HttpResponse(simplejson.dumps(locs), content_type="application/json")
+    import random
+    locs = []
+    for x in range(5):
+        locs.append({
+            "long": random.uniform(-180,180),
+            "lat": random.uniform(-90, 90),
+            "altitude": random.uniform(0, 100),
+            "time_placed": (datetime.datetime.utcnow().replace(tzinfo=utc) - datetime.timedelta(hours=random.randint(0, 120))).isoformat(),
+            "radius": 10,
+            "placed_by": "239r2e90efj%d" % random.randint(5,8),
+        })
+    return HttpResponse(simplejson.dumps(locs), content_type="application/json")
 
 
 # TODO:
@@ -25,25 +27,52 @@ def test(request):
 # - Handle deletion of a game room (if players no longer exist in it)
 # - Handle the first device/user registration
 
+def join_game(request):
+    if request.method == "POST":
+        try:
+            player = Player.objects.get(device_id=request.POST.get('device_id'))
+        except ObjectDoesNotExist:
+            # Return error telling user to register
+            return
+        try:
+            game = Game.objects.get(pk=request.POST.get('game_id'))
+        except ObjectDoesNotExist:
+            # Return error telling that game doesn't exist
+            return
+        player.game = game
+        player.save()
+        return HttpResponse(simplejson.dumps({"success": True, "game": game.pk}), content_type="application/json")
+        
+
 # Returns the bombs within a specified radius of a location
 def get_bombs(request):
-	if request.method == "POST":
-		return HttpResponse(simplejson.dumps({"test": [1,2,3]}), content_type="application/json")
-	else:
-		return HttpResponseBadRequest()
+    if request.method == "POST":
+        bombs = [b.get_info() for b in bombs_in_radius(float(request.POST.get("long")), float(request.POST.get("lat")), float(request.POST.get("rad")))]
+        return HttpResponse(simplejson.dumps(bombs), content_type="application/json")
+    else:
+        return HttpResponseBadRequest()
 
 
 # Handles when a player places a bomb
 def place_bomb(request):
-	if request.method == "POST":
-		return HttpResponse(simplejson.dumps({"test": [1,2,3]}), content_type="application/json")
-	else:
-		return HttpResponseBadRequest()
+    if request.method == "POST":
+        try:
+            player = Player.objects.get(device_id=request.POST.get('device_id'))
+        except ObjectDoesNotExist:
+            # Return error telling user to register
+            return
+        bomb = player.place_bomb(request.POST.get("long"), request.POST.get("lat"), request.POST.get("alt"))
+        if bomb == None:
+            # Error Code 1: Not enough bombs
+            return HttpResponse(simplejson.dumps({"success": False, "error_code": 1}))
+        return HttpResponse(simplejson.dumps({"success": True, "bomb": bomb.get_info()}), content_type="application/json")
+    else:
+        return HttpResponseBadRequest()
 
 
 # Handles when a player is hit by a bomb
 def bomb_player(request):
-	if request.method == "POST":
-		return HttpResponse(simplejson.dumps({"test": [1,2,3]}), content_type="application/json")
-	else:
-		return HttpResponseBadRequest()
+    if request.method == "POST":
+        return HttpResponse(simplejson.dumps({"test": [1,2,3]}), content_type="application/json")
+    else:
+        return HttpResponseBadRequest()
