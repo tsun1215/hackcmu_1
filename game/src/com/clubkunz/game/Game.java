4ee2d5bde0354653e7df78c22644a90a3281dd30
davidlindenbaum <dlindenb@andrew.cmu.edu>
2013-09-14 16:37:49 -0400

    stuff

--- game/src/com/clubkunz/game/Game.java
+++ game/src/com/clubkunz/game/Game.java
@@ -1,12 +1,7 @@
 package com.clubkunz.game;
 
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.location.Criteria;
-import android.location.GpsStatus;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
@@ -14,10 +9,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Game extends Activity {
-	LocationManager location;
-	int updateFreq = 1000;
-	boolean hasLocation = false;
-	Location currentLocation;
+	LocationTracker location = new LocationTracker();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,34 +17,7 @@ public class Game extends Activity {
 
 		setContentView(R.layout.activity_game);
 		
-		location = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		Criteria c = new Criteria();
-		c.setAccuracy(Criteria.ACCURACY_FINE);
-		location.requestLocationUpdates("gps", updateFreq, 0, new LocationListener() {
-			
-			@Override
-			public void onStatusChanged(String provider, int status, Bundle extras) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onProviderEnabled(String provider) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onProviderDisabled(String provider) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onLocationChanged(Location location) {
-				currentLocation = location;
-			}
-		});
+		location.init((LocationManager) getSystemService(Context.LOCATION_SERVICE));
 
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
